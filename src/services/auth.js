4deb69b87c9c39dd6599642f37043cca50f79510
Rishav Bharti <brishabh619@gmail.com>
2021-03-04 23:20:54 +0530

    setted up signin and logout functions in auth

---
+++ src/services/auth.js
@@ -0,0 +1,30 @@
+import { auth, provider } from '../firebase';
+
+export const signInWithGoogle = async () => {
+	let user;
+	await auth
+		.signInWithPopup(provider)
+		.then((res) => {
+			console.log(res.user);
+			user = res.user;
+		})
+		.catch((err) => {
+			console.log(err.message);
+		});
+
+	return user;
+};
+
+export const logOut = async () => {
+	let logout_success;
+	await auth
+		.signOut()
+		.then(() => {
+			logout_success = true;
+		})
+		.catch((err) => {
+			console.log(err.message);
+		});
+
+	return logout_success;
+};
