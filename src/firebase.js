1cb2bbf4366288c3d33b01155822299f4a381d09
Rishav Bharti <brishabh619@gmail.com>
2021-03-04 19:00:26 +0530

    feat: setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyDGe2Ipamd9fWMUZhm427OydcIbtZBDdkw',
+	authDomain: 'rsocial-by-rishav.firebaseapp.com',
+	projectId: 'rsocial-by-rishav',
+	storageBucket: 'rsocial-by-rishav.appspot.com',
+	messagingSenderId: '1782258806',
+	appId: '1:1782258806:web:eac1d59f1ccbaccad85357',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider, storage };
